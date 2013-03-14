53c58735256584c099332d2c28ee43f16091208d
Nikolas Pontikos <npontikos@gmail.com>
2013-03-14 10:48:08 +0000

    Add method to plot ungated BeadFlowFrame with no colour.

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -1,7 +1,7 @@
 
-### Plot the results of the clustering.
-### Plot only the requested channel
-### which should have a corresponding entry in the MEF files
+#' Plot the results of the clustering.
+#' Plot only the requested channel
+#' which should have a corresponding entry in the MEF files
 setMethod('plot',
     signature=signature(x='GatedBeadFlowFrame',y='character'),
     definition=function(x, y, ...) {
@@ -44,7 +44,6 @@ setMethod('plot',
                 #text(2,5, equation, cex=.75)
             } else {
                 #no mef just draw histogram
-                xlab <- substitute(trans.name(p),list(trans.name=bead.data@trans@transformationId, p=p))
                 plot(h,
                      xlim=c(0, max(x)),
                      col='white',
@@ -61,10 +60,44 @@ setMethod('plot',
     }
 )
 
-
-### If no argument specified then plot all parameters
+#'
+#' Ungated bead data, simply draw all channels individually (no colours).
+#'
+#'
 setMethod('plot',
-    signature=signature(x='GatedBeadFlowFrame',y='missing'),
+          signature=signature(x='BeadFlowFrame',y='character'),
+          definition=function(x, y, ...) {
+            bead.data <- x
+            bead.parameters <- y
+            trans <- bead.data@trans
+            trans.name <- bead.data@trans@transformationId            
+            old.par <- par(no.readonly=T)
+            par(mfrow=c(length(bead.parameters), 1), oma=c(0,0,4,0), mar=c(4,3,2,0), mgp=c(2, 1, 0))           
+            for (p in bead.parameters) {
+              x <- trans(bead.data@exprs[,p])
+              h <- hist(x, breaks=1000, plot=F)
+              xlab <- substitute(trans.name(p), list(trans.name=trans.name, p=p) )
+              plot(h,
+                     xlim=c(0, max(x)),
+                     col='white',
+                     xlab='',
+                     ylab=NULL,
+                     main=xlab)
+              segments(x0=h$mids, y0=0, x1=h$mids, y1=h$counts)
+            }
+            title( sprintf('Date: %s', getDate(bead.data)), line=1, outer=T)
+            title( sprintf('Number of beads: %d', length(bead.data)), line=2, outer=T)
+            par(old.par)
+          }
+)
+
+
+
+#'  Plot function for \code{BeadFlowFrame}
+#'
+#'  If no argument specified then plot all parameters
+setMethod('plot',
+    signature=signature(x='BeadFlowFrame',y='missing'),
     definition=function(x, ...) {
         plot(x, getParams(x))
     }
